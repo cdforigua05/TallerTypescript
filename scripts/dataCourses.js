e25e196b468be99e2faddb869666333f127295c8
Cristhian David Forigua Diaz <cd.forigua@uniandes.edu.co>
2020-10-15 23:04:10 -0500

    Taller terminado

---
+++ scripts/dataCourses.js
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW", "César Forero", 3),
+    new Course("Modelado y simulación", "Juan Manuel Cordovez", 3),
+    new Course("Infraestructura computacional", "Harold Castro", 3),
+    new Course("Advanced machine learning", "Pablo Árbelaez", 4),
+    new Course("Practica social", "David Praga", 3),
+    new Course("TI en las organizaciones", "Paola Lara", 3)
+];
